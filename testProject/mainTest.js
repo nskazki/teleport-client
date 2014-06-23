c946b3be7a74485ef62b9ce72dcfd525d474ffd6
nskazki <nskazki@gmail.com>
2014-06-23 23:19:37 +0400

    add test for new features

--- testProject/mainTest.js
+++ testProject/mainTest.js
@@ -15,98 +15,77 @@ requirejs(['TeleportClient', 'util'], function(TeleportClient, util) {
 	window.teleportClient = new TeleportClient({
 		serverAddress: "ws://nskazki.dyndns.info:8000",
 		//serverAddress: "ws://localhost:8000",
-		reconnect: {
-			isUse: true,
-			delay: 3000
-		}
-	});
+		autoReconnect: 3000
+	})
+		.on('debug', console.debug.bind(console))
+		.on('info', console.info.bind(console))
+		.on('warn', console.warn.bind(console))
+		.on('error', console.error.bind(console))
+		.on('ready', function(objectsProps) {
+			//for Debuging
+			window.simpleObject = teleportClient.objects.simpleObject;;
 
-	(function initTeleportClient() {
+			/*events*/
+			simpleObject
+				.on(
+					'eventWithMyOptions',
+					CreateEventLogger('simpleObject', 'eventWithMyOptions'))
+				.on(
+					'eventWithoutArgs',
+					CreateEventLogger('simpleObject', 'eventWithoutArgs'))
+				.on(
+					'eventWithUnlimArgs',
+					CreateEventLogger('simpleObject', 'eventWithUnlimArgs'))
+				.on(
+					'10secIntervalEvent',
+					CreateEventLogger('simpleObject', '10secIntervalEvent'));
 
-		teleportClient
-			.on('debug', console.debug.bind(console))
-			.on('info', console.info.bind(console))
-			.on('warn', console.warn.bind(console))
-			.on('error', console.error.bind(console))
-			.on('close', function() {
-				// console.warn({
-				// 	desc: "[main] Warn: Соединение с TeleportServer закрылось, будет выполненно переподключение.",
-				// 	serverAddress: "ws://nskazki.dyndns.info:8000",
-				// 	delay: 1000*20
-				// });
 
-				// setTimeout(initTeleportClient, 1000*20);
-			})
-		// .on(
-		// 	'ready',
-		// 	CreateEventLogger('teleportClient', 'ready'))
-		.on(
-			'ready',
-			function(objectsProps) {
-				//for Debuging
-				window.simpleObject = teleportClient.objects.simpleObject;;
+			/*funcs with callback*/
+			simpleObject
+				.func(
+					'simepleParam',
+					CreateCallbackLogger('simpleObject', 'func'))
+				.funcWithoutArgs(
+					CreateCallbackLogger('simpleObject', 'funcWithoutArgs'))
 
-				// var params = {
-				// 	simple: 'object',
-				// 	with: 'some',
-				// 	param: ', wow!'
-				// };
+			/*funcs without callback*/
+			simpleObject
+				.funcWithUnlimArgs(false, '1', 2, 3)
+				.funcWith10SecDelay();
 
-				// /*event*/
-				// simpleObject
-				// 	.on(
-				// 		'eventWithMyOptions',
-				// 		CreateEventLogger('simpleObject', 'eventWithMyOptions'))
-				// 	.on(
-				// 		'eventWithoutArgs',
-				// 		CreateEventLogger('simpleObject', 'eventWithoutArgs'))
-				// 	.on(
-				// 		'eventWithUnlimArgs',
-				// 		CreateEventLogger('simpleObject', 'eventWithUnlimArgs'))
-				// 	.on(
-				// 		'10secIntervalEvent',
-				// 		CreateEventLogger('simpleObject', '10secIntervalEvent'));
+		}).init();
 
-				// /*funcs with callback*/
-				// simpleObject
-				// 	.simpleFunc(
-				// 		params,
-				// 		CreateCallbackLogger('simpleObject', 'simpleFunc'))
-				// 	.simpleFuncWithUnlimArgs(
-				// 		false, '1', 2, {
-				// 			3: new Date()
-				// 		},
-				// 		CreateCallbackLogger('simpleObject', 'simpleFuncWithUnlimArgs'))
-				// 	.simpleFuncWithoutArgs(
-				// 		CreateCallbackLogger('simpleObject', 'simpleFuncWithoutArgs'))
-				// 	.simpleFuncWith10SecDelay(
-				// 		CreateCallbackLogger('simpleObject', 'simpleFuncWith10SecDelay'));
-
-				// /*funcs without callback*/
-				// simpleObject
-				// 	.simpleFunc(params)
-				// 	.simpleFuncWithUnlimArgs(false, '1', 2, 3)
-				// 	.simpleFuncWithoutArgs()
-				// 	.simpleFuncWith10SecDelay();
-			}).init();
-	})();
 
 	function CreateEventLogger(objectName, eventName) {
 		return function() {
-			console.log({
-				desc: util.format("[%s.event] Info: получено событие с сервера %s", objectName, eventName),
+			var obj = {
+				desc: util.format("[%s.event] Info: получено событие %s", objectName, eventName),
 				arguments: arguments
-			});
+			};
+
+			var details = document.createElement('details');
+			details.innerHTML = "<summary>" + obj.desc + "</summary>" + "<pre>" + JSON.stringify(obj, ' ', 4) + "</pre";
+
+			var welcom = document.getElementById('welcom');
+			welcom.parentNode.insertBefore(details, welcom);
 		}
 	}
 
 	function CreateCallbackLogger(objectName, methodName) {
 		return function(error, result) {
-			console.log({
+			var obj = {
 				desc: util.format("[%s.callback] Info: вернулся результат вызова метода %s", objectName, methodName),
 				result: result,
 				error: error
-			})
+			};
+
+
+			var details = document.createElement('details');
+			details.innerHTML = "<summary>" + obj.desc + "</summary>" + "<pre>" + JSON.stringify(obj, ' ', 4) + "</pre";
+
+			var welcom = document.getElementById('welcom');
+			welcom.parentNode.insertBefore(details, welcom);
 		}
 	}
 });
